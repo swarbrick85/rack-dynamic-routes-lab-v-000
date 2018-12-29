be04d3162cfece3b35dae6ac072a78522fee5624
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-29 19:32:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,6 @@
 require 'pry'
 
 class Application
-
-  @@cart = []
-  
-  @@items = [Item.new("avocado", 7.39), Item.new("apple", 0.75)]
   
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +9,15 @@ class Application
     if !req.path.match(/items/)
       resp.write "Route not found"
       resp.status = 404
+    elsif req.path.match(/items/)
+      item = req.env["PATH_INFO"].split("/items/")[1]
+      binding.pry
+      if (@@items.find {|itemm| itemm.name == item})
+        resp.write "#{(@@items.find {|itemm| itemm.name == item}).price}"
+      else 
+        resp.write "Item not found"
+      end 
     end 
+    resp.finish
   end 
 end
